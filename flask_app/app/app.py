84d2b832ebdcd14b1153342a826001574f15f435
Shvd <2100992@gmail.com>
2020-03-08 01:06:46 +0300

    experiment

--- flask_app/app/app.py
+++ flask_app/app/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
-@app.route('/')
+@app.route('/flask/')
 def hello_world():
     return 'Hello, Docker! (from Flask)'
 

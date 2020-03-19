f94b77c3fe25757cce306297c4ea2ae9e965e9ee
Shvd <2100992@gmail.com>
2020-03-19 18:05:10 +0300

    small fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from apps.app import app
 from apps import view
-from apps.config import HOST,PORT,DEBUG
+from apps.config import HOST,PORT
 
 
 if __name__ == '__main__':
-    app.run(host=HOST, port=PORT, debug=DEBUG)
+    app.run(host=HOST, port=PORT)
